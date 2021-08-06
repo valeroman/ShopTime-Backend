aeff8028645ebe6a41f2c1341b8af51138d15273
Roman Valero <valeroman@gmail.com>
2021-08-06 09:33:13 -0500

    Authentication

---
+++ shop_time/urls.py
@@ -0,0 +1,16 @@
+from django import urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.social.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^.*', 
+                        TemplateView.as_view(template_name='index.html'))]
